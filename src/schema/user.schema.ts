3579bd37ccecc75e2e1e5f72e6f990943f927bad
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-28 18:20:27 +0600

    create user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,36 @@
+import { Schema } from "mongoose";
+import { User } from "../interface/user.interface";
+const userSchema = new Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      maxlength: 50,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export { userSchema };
