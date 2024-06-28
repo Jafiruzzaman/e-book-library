5adf67d1d5d92f9603e976d10ca7301ee43839ed
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-28 18:52:35 +0600

    create signUp validation schema

---
+++ src/validations/auth.validationj.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+const signupSchema = z.object({
+  username: z
+    .string({ required_error: "user name is required" })
+    .trim()
+    .min(3, "user name must be at least 3 character")
+    .max(50, "user name must not be more than 50 character"),
+  email: z
+    .string({ required_error: "email is required" })
+    .email()
+    .trim()
+    .min(3, "email must be at least 3 character")
+    .max(50, "email must not be more than 50 character"),
+  password: z
+    .string({ required_error: "password is required" })
+    .trim()
+    .min(8, "password must be at least 8 character")
+    .max(50, "password must not be more than 50 character"),
+});
+export { signupSchema };
