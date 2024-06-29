cbd949aea19281744ef6b2402f34a73ffe074218
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-06-29 14:54:19 +0600

    add contact validation

---
+++ src/validations/contact.validation.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+const ContactSchema = z.object({
+  userName: z.string({ required_error: "username is required" }).trim(),
+  email: z.string({ required_error: "email is required" }).trim().email(),
+  message: z.string({ required_error: "message is required" }).trim(),
+});
+export { ContactSchema };
